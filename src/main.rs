cd936fa3cebad079b62d5af884740b5385f7b03e
studokim <studokim@gmail.com>
2024-01-07 20:04:08 +0300

    Add env parsing, logging

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,37 @@ use std::{
     str::FromStr,
 };
 
+mod log;
 mod rest;
 
 #[derive(Parser, Debug)]
 #[command(version, about = "Приложение к празднику Единения 2024", long_about = None)]
 struct Args {
-    #[arg(long, default_value_t = IpAddr::from_str("127.0.0.1")
-                                  .expect("Invalid default IP set. Need to recompile the program."))]
-    ip: IpAddr,
+    #[arg(env, long, default_value_t = IpAddr::from_str("127.0.0.1")
+    .expect("Invalid default IP set. Need to recompile the program."))]
+    api_ip: IpAddr,
 
-    #[arg(short, long, value_parser = clap::value_parser!(u16).range(1024..65535))]
-    port: u16,
+    #[arg(env, short = 'p', long, value_parser = clap::value_parser!(u16).range(1024..65535))]
+    api_port: u16,
+
+    #[arg(env, short, long)]
+    database_url: Option<String>,
 }
 
 #[tokio::main]
 async fn main() {
+    log::configure();
+
     let args = Args::parse();
-    let addr = SocketAddr::from((args.ip, args.port));
+    if args.database_url == None {
+        log::warn!("DATABASE_URL is not set, limited functionality")
+    }
+    let addr = SocketAddr::from((args.api_ip, args.api_port));
 
     let listener = tokio::net::TcpListener::bind(addr)
         .await
         .expect("Cannot bind the requested address");
-    println!("Listening on {} ...", addr);
+    log::info!("Listening on {} ...", addr);
 
     let router = rest::router::new();
     axum::serve(listener, router)
--- src/rest/handlers.rs
+++ src/rest/handlers.rs
@@ -1,6 +1,9 @@
 use axum::response::Html;
 
+use crate::log;
+
 pub async fn index() -> Html<&'static str> {
+    log::info!("index.html");
     // Include an utf-8 file contents as `&'static str` in compile time.
     // This macro is relative to current `handlers.rs` file.
     Html(include_str!("../../html/index.html"))
