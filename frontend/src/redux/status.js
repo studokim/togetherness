4e3c89906dcdcb63c006a05f44f40abba4fd4bb3
Ololot <you@example.com>
2024-02-10 00:11:01 +0300

    #42

--- frontend/src/redux/status.js
+++ frontend/src/redux/status.js
@@ -170,7 +170,7 @@ export const status = createSlice({
             const timer = res.data.seconds;
             action.payload.callback(timer);
           }
-          if (res.data.error === "notStarted") { //игра не началась
+          if (res.data.error === "NotStarted") { //игра не началась
             console.log("ngetTimer otStarted");
             action.payload.callback(-1);
           }
