5dcdfa06c0e538f2d9b3a3931632c2ef3f6fa2d4
studokim <studokim@gmail.com>
2024-02-09 21:34:54 +0300

    Fix dubious ownership warning

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,10 +6,10 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 COMMIT_BEFORE="$(sudo -u kim git log --pretty=format:'%h' -n 1)"
-sudo -u kim \
-git stash save && \
-git pull >/dev/null && \
-git stash pop
+sudo -u kim bash -c \
+"git stash save && \
+GIT_SSH_COMMAND='ssh -i /home/kim/.ssh/id_rsa' git pull >/dev/null && \
+git stash pop"
 COMMIT_AFTER="$(sudo -u kim git log --pretty=format:'%h' -n 1)"
 
 if [ "$COMMIT_BEFORE" == "$COMMIT_AFTER" ]; then
